681a178c2cd128d719756f4f3cd35b3cd4fe696c
Vinicius Sanches <viniciuus.sanches.ramos@gmail.com>
2025-11-27 19:59:48 -0300

    chore: spring boot setup project

---
+++ src/main/java/br/edu/utfpr/td/tsi/trabalho2apiswebservices/Trabalho2apiswebservicesApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.td.tsi.trabalho2apiswebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabalho2apiswebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trabalho2apiswebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/utfpr/td/tsi/trabalho2apiswebservices/Trabalho2apiswebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.td.tsi.trabalho2apiswebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trabalho2apiswebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
