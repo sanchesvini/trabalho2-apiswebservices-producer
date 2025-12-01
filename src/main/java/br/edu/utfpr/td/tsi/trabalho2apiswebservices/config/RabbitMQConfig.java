7c4cd775f44bfd8985e2b9080ba606151213a3db
Vinicius Sanches <viniciuus.sanches.ramos@gmail.com>
2025-12-01 09:41:44 -0300

    feat: implementa configuração para o RabbitMQ

---
+++ src/main/java/br/edu/utfpr/td/tsi/trabalho2apiswebservices/config/RabbitMQConfig.java
@@ -0,0 +1,18 @@
+package br.edu.utfpr.td.tsi.trabalho2apiswebservices.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String QUEUE_NAME = "transacoes.financeiras";
+
+    @Bean
+    public Queue queue() {
+        // Define a fila transacoes.financeiras como durável [cite: 19]
+        return new Queue(QUEUE_NAME, true);
+    }
+
+}
