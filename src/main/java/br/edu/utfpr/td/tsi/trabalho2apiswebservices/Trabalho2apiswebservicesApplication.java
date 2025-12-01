bbc07f9a52f9718cd64adff7da83cb23ff858c52
Vinicius Sanches <viniciuus.sanches.ramos@gmail.com>
2025-12-01 09:47:04 -0300

    feat: implementa método init para inicialização do transacaoProducer

--- src/main/java/br/edu/utfpr/td/tsi/trabalho2apiswebservices/Trabalho2apiswebservicesApplication.java
+++ src/main/java/br/edu/utfpr/td/tsi/trabalho2apiswebservices/Trabalho2apiswebservicesApplication.java
@@ -1,13 +1,24 @@
 package br.edu.utfpr.td.tsi.trabalho2apiswebservices;
 
+import br.edu.utfpr.td.tsi.trabalho2apiswebservices.producer.TransacaoProducer;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Trabalho2apiswebservicesApplication {
 
+	@Autowired
+	private TransacaoProducer transacaoProducer;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Trabalho2apiswebservicesApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		// Inicia o produtor de transações após a aplicação ser iniciada
+		transacaoProducer.lerCsv();
+	}
 }
